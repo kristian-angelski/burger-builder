1c2d948c81bc40daa337c9593c85132cc012b62a
Kristian Angelski <kange@softserveinc.com>
2019-05-15 23:09:43 +0300

    adding purchaseCancelHandler, which is being passed by props to Modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,6 +75,10 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: true });
 	};
 
+	purchaseCancelHandler = () => {
+		this.setState({ purchasing: false });
+	};
+
 	render() {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -85,7 +89,10 @@ class BurgerBuilder extends Component {
 
 		return (
 			<Auxiliary>
-				<Modal show={this.state.purchasing}>
+				<Modal
+					modalClosed={this.purchaseCancelHandler}
+					show={this.state.purchasing}
+				>
 					<OrderSummary ingredients={this.state.ingredients} />
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
