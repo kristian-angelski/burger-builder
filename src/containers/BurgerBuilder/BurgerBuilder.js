9e107264e693bdb58727951f2e272c925eb853de
kristian.angelski <kristian.angelski@gmail.com>
2019-05-15 11:40:08 +0300

    adding BurgerBuilder statefull container-component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+	render() {
+		return (
+			<Auxiliary>
+				<div>Burger - graphical representation</div>
+				<div>Build controls</div>
+			</Auxiliary>
+		);
+	}
+}
+
+export default BurgerBuilder;
