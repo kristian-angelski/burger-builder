7f667c209042b1af9c616f88995d02966e9a7122
Kristian Angelski <kange@softserveinc.com>
2019-05-16 00:08:46 +0300

    importing the NavigationItems into the Toolbar component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = props => {
 	return (
@@ -9,7 +10,7 @@ const toolbar = props => {
 			{/* placeholders for now - add components instead*/}
 			<div>MENU</div>
 			<Logo />
-			<nav>...</nav>
+			<NavigationItems />
 		</header>
 	);
 };
