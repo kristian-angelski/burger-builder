cb97ada0f1281439e769245c4e15c2d54d5c9219
Kristian Angelski <kange@softserveinc.com>
2019-05-16 01:17:10 +0300

    adding state to the Layout component and a handler which are being passed as proprs down to SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,11 +6,22 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
+	state = {
+		showSideDrawer: true
+	};
+
+	sideDrawerClosedHandler = () => {
+		this.setState({ showSideDrawer: false });
+	};
+
 	render() {
 		return (
 			<Auxiliary>
 				<Toolbar />
-				<SideDrawer />
+				<SideDrawer
+					open={this.state.showSideDrawer}
+					closed={this.sideDrawerClosedHandler}
+				/>
 				<main className={classes.Content}>{this.props.children}</main>
 			</Auxiliary>
 		);
