067bd14f2845439819ed1d328ccd042b84221009
kristian.angelski <kristian.angelski@gmail.com>
2019-05-15 15:21:26 +0300

    transforming the ingredients object to an Array - [transformedIngredients] and rendering it

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = props => {
+	const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
+		return [...Array(props.ingredients[ingKey])].map((_, i) => {
+			return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+		});
+	});
+
+	return (
+		<div className={classes.Burger}>
+			<BurgerIngredient type="bread-top" />
+			{transformedIngredients}
+			<BurgerIngredient type="bread-bottom" />
+		</div>
+	);
+};
+
+export default burger;
