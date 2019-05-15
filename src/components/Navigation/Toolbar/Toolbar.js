83c1fbdef5811a6c1c187ddfbbbe77867d26d322
Kristian Angelski <kange@softserveinc.com>
2019-05-15 23:40:32 +0300

    adding static toolbar on top of screen with some stylings

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+
+const toolbar = props => {
+	return (
+		<header className={classes.Toolbar}>
+			{/* placeholders for now - add components instead*/}
+			<div>MENU</div>
+			<div>LOGO</div>
+			<nav>...</nav>
+		</header>
+	);
+};
+
+export default toolbar;
