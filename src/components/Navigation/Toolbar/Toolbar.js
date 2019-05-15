0c172cdd24fa9a233db28ef8e1f2cfd8e5ab20a2
Kristian Angelski <kange@softserveinc.com>
2019-05-16 01:27:32 +0300

    importing DrawerToggle inot the Toolbar, setting clicked prop to expect a function which will be handled in the Layout component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,12 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = props => {
 	return (
 		<header className={classes.Toolbar}>
-			{/* placeholders for now - add components instead*/}
-			<div>MENU</div>
+			<DrawerToggle clicked={props.drawerToggleClick} />
 			<div className={classes.Logo}>
 				<Logo />
 			</div>
