ea102591c573b2b02677d2d7e37842b0765c8900
Kristian Angelski <kange@softserveinc.com>
2019-05-15 22:44:36 +0300

    adding OrderSummary, which will be passed to the MODAL component

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Auxiliary from '../../../hoc/Auxiliary';
+
+const orderSummary = props => {
+	const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
+		return (
+			// console.log(ingKey, props.ingredients[ingKey])
+
+			<li key={ingKey}>
+				<span style={{ textTransform: 'capitalize' }}>{ingKey}</span>:{' '}
+				{props.ingredients[ingKey]}
+			</li>
+		);
+	});
+
+	return (
+		<Auxiliary>
+			<h3>Your Order</h3>
+			<p>A delicious burger with the following ingredients:</p>
+			<ul>{ingredientSummary}</ul>
+			<p>Continue to Checkout?</p>
+		</Auxiliary>
+	);
+};
+
+export default orderSummary;
