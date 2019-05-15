bb7bad9d04bb2a419853500ed92c7d7295a98da8
kristian.angelski <kristian.angelski@gmail.com>
2019-05-15 11:23:21 +0300

    adding layout component into root(app) component; changing quotes to single

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import Auxiliary from "../../hoc/Auxiliary";
+import Auxiliary from '../../hoc/Auxiliary';
 
 const layout = props => (
-  <Auxiliary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
-  </Auxiliary>
+	<Auxiliary>
+		<div>Toolbar, SideDrawer, Backdrop</div>
+		<main>{props.children}</main>
+	</Auxiliary>
 );
 
 export default layout;
