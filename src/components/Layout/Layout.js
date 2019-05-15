b017acb271870152aa25ad6b24983a49fd57b552
kristian.angelski <kristian.angelski@gmail.com>
2019-05-15 11:42:53 +0300

    using css modules, we add margin to the layout component so that the main content can be separated from the navigation placeholder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.css';
 
 const layout = props => (
 	<Auxiliary>
-		<div>Toolbar, SideDrawer, Backdrop</div>
-		<main>{props.children}</main>
+		<div>Placeholder for Toolbar, SideDrawer, Backdrop</div>
+		<main className={classes.Content}>{props.children}</main>
 	</Auxiliary>
 );
 
