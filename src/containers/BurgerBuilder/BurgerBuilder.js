30b012774fd4528bb84f57e548b8e329ea1e8c81
kristian.angelski <kristian.angelski@gmail.com>
2019-05-16 14:19:11 +0300

    adding post request for ordering the burger via continue button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -45,6 +46,7 @@ class BurgerBuilder extends Component {
 			...this.state.ingredients
 		};
 		updatedIngredients[type] = updatedCount;
+		console.log(updatedIngredients);
 		const priceAddition = INGREDIENT_PRICES[type];
 		const oldPrice = this.state.totalPrice;
 		const newPrice = oldPrice + priceAddition;
@@ -80,7 +82,25 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseContinueHandler = () => {
-		alert('You continue!');
+		// alert('You continue!');
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: 'Kristian Angelski',
+				address: {
+					street: 'Teststreet 1',
+					zipCode: '41414',
+					country: 'Bulgaria'
+				},
+				email: 'test@test.com'
+			},
+			deliveryMethod: 'fastest'
+		};
+		axios
+			.post('/orders.json', order)
+			.then(response => console.log(response))
+			.catch(error => console.log(error));
 	};
 
 	render() {
