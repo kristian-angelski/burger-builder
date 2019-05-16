7706c3186790d084a3ed75c358296aca7ff9f02f
kristian.angelski <kristian.angelski@gmail.com>
2019-05-16 15:54:29 +0300

    we now fetch the ingredients from firebase, and because of that, everything that depends on the ingredients is not being shown initially, but only if the ingredients are already loaded

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,17 +18,24 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component {
 	state = {
-		ingredients: {
-			salad: 0,
-			bacon: 0,
-			cheese: 0,
-			meat: 0
-		},
+		ingredients: null,
 		totalPrice: 4,
 		purchaseable: false,
 		purchasing: false,
-		loading: false
+		loading: false,
+		error: false
 	};
+
+	componentDidMount() {
+		axios
+			.get('https://react-burger-builder-4d131.firebaseio.com/ingredients.json')
+			.then(response => {
+				this.setState({ ingredients: response.data });
+			})
+			.catch(error => {
+				this.setState({ error: true });
+			});
+	}
 
 	updatePurchaseState(ingredients) {
 		const sum = Object.keys(ingredients)
@@ -118,14 +125,38 @@ class BurgerBuilder extends Component {
 		for (let key in disabledInfo) {
 			disabledInfo[key] = disabledInfo[key] <= 0;
 		}
-		let orderSummary = (
-			<OrderSummary
-				price={this.state.totalPrice}
-				purchaseCanceled={this.purchaseCancelHandler}
-				purchaseContinued={this.purchaseContinueHandler}
-				ingredients={this.state.ingredients}
-			/>
+		let orderSummary = null;
+
+		let burger = this.state.error ? (
+			<p>Ingredients can't be loaded!</p>
+		) : (
+			<Spinner />
 		);
+		if (this.state.ingredients) {
+			burger = (
+				<Auxiliary>
+					<Burger ingredients={this.state.ingredients} />
+					<BuildControls
+						ingredientAdded={this.addIngredientHandler}
+						ingredientRemoved={this.removeIngredientHandler}
+						totalPrice={this.state.totalPrice}
+						disabled={disabledInfo}
+						purchaseable={this.state.purchaseable}
+						ordered={this.purchaseHandler}
+					/>
+				</Auxiliary>
+			);
+
+			orderSummary = (
+				<OrderSummary
+					price={this.state.totalPrice}
+					purchaseCanceled={this.purchaseCancelHandler}
+					purchaseContinued={this.purchaseContinueHandler}
+					ingredients={this.state.ingredients}
+				/>
+			);
+		}
+
 		if (this.state.loading) {
 			orderSummary = <Spinner />;
 		}
@@ -138,15 +169,7 @@ class BurgerBuilder extends Component {
 				>
 					{orderSummary}
 				</Modal>
-				<Burger ingredients={this.state.ingredients} />
-				<BuildControls
-					ingredientAdded={this.addIngredientHandler}
-					ingredientRemoved={this.removeIngredientHandler}
-					totalPrice={this.state.totalPrice}
-					disabled={disabledInfo}
-					purchaseable={this.state.purchaseable}
-					ordered={this.purchaseHandler}
-				/>
+				{burger}
 			</Auxiliary>
 		);
 	}
