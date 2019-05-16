86a753a8f0978c50cf75f15aed77567a62bd3e82
kristian.angelski <kristian.angelski@gmail.com>
2019-05-16 15:09:23 +0300

    fixing a problem with the OrderSummary, which was preveneting the actual count to be shown

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,26 +5,28 @@ import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
 	// This could be a functional component, doesn't have to be a class - it is a class so that lifecycle methods could be used
-	ingredientSummary = Object.keys(this.props.ingredients).map(ingKey => {
-		return (
-			<li key={ingKey}>
-				<span style={{ textTransform: 'capitalize' }}>{ingKey}</span>:{' '}
-				{this.props.ingredients[ingKey]}
-			</li>
-		);
-	});
-
 	// using this for debugging - to check for bottle neck like unnecessary re-renders
 	componentWillUpdate() {
 		console.log('[OrderSummary] willUpdate');
 	}
 
 	render() {
+		const ingredientSummary = Object.keys(this.props.ingredients).map(
+			ingKey => {
+				return (
+					<li key={ingKey}>
+						<span style={{ textTransform: 'capitalize' }}>{ingKey}</span>:{' '}
+						{this.props.ingredients[ingKey]}
+					</li>
+				);
+			}
+		);
+
 		return (
 			<Auxiliary>
 				<h3>Your Order</h3>
 				<p>A delicious burger with the following ingredients:</p>
-				<ul>{this.ingredientSummary}</ul>
+				<ul>{ingredientSummary}</ul>
 				<p>
 					<strong>Total Price: {this.props.price.toFixed(2)}</strong>
 				</p>
