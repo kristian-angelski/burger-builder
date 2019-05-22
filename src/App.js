7696c378f98764b228aea6b0cf52bb766bcf8c3c
kristian.angelski <kristian.angelski@gmail.com>
2019-05-22 14:45:07 +0300

    wrapping whole app with the BrowserRouter component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,9 +8,11 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<Layout>
-					<BurgerBuilder />
-				</Layout>
+				<BrowserRouter>
+					<Layout>
+						<BurgerBuilder />
+					</Layout>
+				</BrowserRouter>
 			</div>
 		);
 	}
