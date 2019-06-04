90db129712c038813598062851de988121be1404
kristian.angelski <kristian.angelski@gmail.com>
2019-06-04 16:56:49 +0300

    adding easier way for transforming OBJ to ARR then the one present in Burger.js, also with the ingredients now present as ARR, they are being outputed on the Orders screen

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,13 +2,42 @@ import React from 'react';
 
 import classes from './Order.css';
 
-const order = props => (
-	<div className={classes.Order}>
-		<p>Ingredients: Salad (1)</p>
-		<p>
-			Price: <strong>USD 5.45</strong>
-		</p>
-	</div>
-);
+const order = props => {
+	const ingredientsArr = [];
+	// converting obj to ARR
+	for (let ingredientName in props.ingredients) {
+		ingredientsArr.push({
+			name: ingredientName,
+			amount: props.ingredients[ingredientName]
+		});
+	}
+	// console.log(ingredientsArr);
+
+	const ingredientOutput = ingredientsArr.map(ig => {
+		return (
+			<span
+				key={ig.name}
+				style={{
+					textTransform: 'capitalize',
+					display: 'inline-block',
+					margin: '0 8px',
+					border: '1px solid #ccc',
+					padding: '5px'
+				}}
+			>
+				{ig.name} ({ig.amount})
+			</span>
+		);
+	});
+
+	return (
+		<div className={classes.Order}>
+			<p>Ingredients: {ingredientOutput}</p>
+			<p>
+				Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
+			</p>
+		</div>
+	);
+};
 
 export default order;
