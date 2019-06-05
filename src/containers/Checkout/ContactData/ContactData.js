76330aed61c073658fcc0f0991fc9973ad5fa316
kristian.angelski <kristian.angelski@gmail.com>
2019-06-05 14:37:19 +0300

    setting up JS Config in ContactData for the form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,57 @@ import axios from '../../../axios-orders';
 
 class ContactData extends Component {
 	state = {
-		name: '',
-		email: '',
-		address: {
-			street: '',
-			postalCode: ''
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name'
+				},
+				value: ''
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street'
+				},
+				value: ''
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'ZIP Code'
+				},
+				value: ''
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country'
+				},
+				value: ''
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your E-mail'
+				},
+				value: ''
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{ value: 'fastest', displayValue: 'Fastest' },
+						{ value: 'cheapest', displayValue: 'Cheapest' }
+					]
+				},
+				value: ''
+			}
 		},
 		loading: false
 	};
@@ -24,17 +70,7 @@ class ContactData extends Component {
 		this.setState({ loading: true });
 		const order = {
 			ingredients: this.props.ingredients,
-			price: this.props.price,
-			customer: {
-				name: 'Kristian Angelski',
-				address: {
-					street: 'Teststreet 1',
-					zipCode: '41414',
-					country: 'Bulgaria'
-				},
-				email: 'test@test.com'
-			},
-			deliveryMethod: 'fastest'
+			price: this.props.price
 		};
 		axios
 			.post('/orders.json', order)
@@ -50,12 +86,7 @@ class ContactData extends Component {
 	render() {
 		let form = (
 			<form>
-				<Input
-					inputtype="input"
-					type="text"
-					name="name"
-					placeholder="Your name"
-				/>
+				<Input elementType="..." elementConfig="..." value="..." />
 				<Input
 					inputtype="input"
 					type="email"
