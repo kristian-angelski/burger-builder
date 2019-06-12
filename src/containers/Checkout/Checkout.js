f34750fc152272e8c5c02ac05935983bd737af2b
kristian.angelski <kristian.angelski@gmail.com>
2019-06-12 15:47:46 +0300

    connecting the redux store to the Checkout container component, removing the componentWillMount which was taking the ingredients from the search params(URL) and instead use the ingredients from store through this.props

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,32 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-	state = {
-		ingredients: null,
-		price: 0
-	};
-
-	componentWillMount() {
-		const query = new URLSearchParams(this.props.location.search);
-		const ingredients = {};
-		let price = 0;
-		for (let param of query.entries()) {
-			// ['salad', '1']
-			// adding + infront of param[1] to make it number
-			if (param[0] === 'price') {
-				price = param[1];
-			} else {
-				ingredients[param[0]] = +param[1];
-			}
-		}
-
-		this.setState({ ingredients: ingredients, price: price });
-	}
-
 	checkoutContinuedHandler = () => {
 		this.props.history.replace('/checkout/contact-data');
 	};
@@ -41,22 +20,22 @@ class Checkout extends Component {
 				<CheckoutSummary
 					checkoutContinued={this.checkoutContinuedHandler}
 					checkoutCancelled={this.checkoutCancelledHandler}
-					ingredients={this.state.ingredients}
-					price={this.state.price}
+					ingredients={this.props.ings}
 				/>
 				<Route
 					path={this.props.match.path + '/contact-data'}
-					render={props => (
-						<ContactData
-							ingredients={this.state.ingredients}
-							price={this.state.price}
-							{...props}
-						/>
-					)}
+					component={ContactData}
 				/>
 			</div>
 		);
 	}
 }
 
-export default Checkout;
+const mapStateToProps = state => {
+	return {
+		ings: state.ingredients,
+		price: state.totalPrice
+	};
+};
+
+export default connect(mapStateToProps)(Checkout);
