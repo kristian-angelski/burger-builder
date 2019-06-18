17a86e15863a07337c935e19435911abe78c1053
kristian.angelski <kristian.angelski@gmail.com>
2019-06-18 16:15:31 +0300

    adding ORDERS action types and action creators

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -11,23 +11,6 @@ class Orders extends Component {
 	};
 
 	componentDidMount() {
-		axios
-			.get('/orders.json')
-			.then(res => {
-				let fetchedOrders = [];
-				for (let key in res.data) {
-					fetchedOrders.push({
-						...res.data[key],
-						id: key
-					});
-				}
-				this.setState({ loading: false, orders: fetchedOrders });
-				console.log(fetchedOrders);
-			})
-			.catch(err => {
-				this.setState({ loading: false });
-			});
-
 		// console.log(this.state.orders);
 	}
 
